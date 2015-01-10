52cd306e5121152640e3d73667471103f023b7ab
hazumu <stellarfishing@gmail.com>
2015-01-10 17:42:18 +0900

    比較js

--- public/js/moe_competition.js
+++ public/js/moe_competition.js
@@ -1,45 +1,17 @@
 // 気が向いたらちゃんと書く＼(^o^)／
 ;(function() {
+
+  var data = JSON.parse($('#js-json-mymoe').html());
+  console.log(data);
+
   // ダミーだよ
   // 中身は違うとこんな配列で帰ってくると素敵
-  var data = [
-    {
-      category: 1,
-      data: [
-        {
-          id: 1,
-          name: 'ほげ'
-        },
-        {
-          id: 2,
-          name: 'ふが'
-        },
-        {
-          id: 3,
-          name: 'ぴよ'
-        },
-        {
-          id: 3,
-          name: 'にゃん'
-        }
-      ]
-    },
-    {
-      category: 2,
-      data: [
-        {
-          id: 1,
-          name: 'クソ'
-        }
-      ]
-    },
-  ];
-
   var Scene = function (data, model) {
     this.data = data;
     this.model = model;
     this.$left = $('#js-block-left');
     this.$right = $('#js-block-right');
+    this.$categoryName = $('#js-text-cat-name');
     this.addEvent();
     this.currentCategoryIdx = 0;
     this.result = [];
@@ -65,7 +37,7 @@
       this.compare(e);
 
       // 選択肢が一つの場合はnextを呼んでカテゴリを進める
-      if (this.getCurrentCategory().data.length <= 1) {
+      if (this.getCurrentCategory().entries.length <= 1) {
         this.goNextCategory();
       }
 
@@ -75,13 +47,13 @@
     // 配列を整形するところまでやるか
     compare: function(e) {
       // クリックした方に仕込まれてるIDは負けた方のID
-      var id = $(e.target).data('lost-id');
-      var data = this.getCurrentCategory().data;
+      var entryId = $(e.target).data('lost-id');
+      var entries = this.getCurrentCategory().entries;
       
       // 削除
       // TODO:いけてないので動いたら治す
-      this.getCurrentCategory().data = data.filter(function(val){
-        return +val.id !== +id;
+      this.getCurrentCategory().entries = entries.filter(function(val){
+        return +val.entryId !== +entryId;
       });
     },
 
@@ -89,18 +61,20 @@
       var competitor = this.getCompetitor();
 
       this.$left
-        .text(competitor[0].name)
-        .data('lost-id', competitor[1].id);
+        .text(competitor[0].entryName)
+        .data('lost-id', competitor[1].entryId);
       this.$right
-        .text(competitor[1].name)
-        .data('lost-id', competitor[0].id);
+        .text(competitor[1].entryName)
+        .data('lost-id', competitor[0].entryId);
+      this.$categoryName.text(this.getCurrentCategory().categoryName);
+
     },
 
     // 色々やらせすぎ
     goNextCategory: function() {
       // データがひとつしかなかったら結界に保存
-      if (this.getCurrentCategory().data.length <= 1) {
-        this.result.push(this.getCurrentCategory().data[0]);
+      if (this.getCurrentCategory().entries.length <= 1) {
+        this.result.push(this.getCurrentCategory().entries[0]);
       }
 
       this.currentCategoryIdx++;
@@ -111,16 +85,14 @@
       }
 
       // データがひとつしかなかったら一つ飛ばし
-      if (this.getCurrentCategory().data.length <= 1) {
-        // 結果に保存
-        this.result.push(this.getCurrentCategory().data[0]);
+      if (this.getCurrentCategory().entries.length <= 1) {
         this.goNextCategory();
       }
     },
 
     getCompetitor: function() {
       // ２つ以上あることを保証した上で実行
-      var suffled = _.shuffle(this.getCurrentCategory().data);
+      var suffled = _.shuffle(this.getCurrentCategory().entries);
       return [suffled[0], suffled[1]];
     },
 

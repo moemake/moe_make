c64e984352687b6e484488349bfe6a65db8848da
hazumu <stellarfishing@gmail.com>
2015-01-24 12:16:08 +0900

    勝ち

---
+++ public/js/modal.js
@@ -0,0 +1,90 @@
+;(function(window, $, undefined) {
+
+  var overlay = $('#js-overlay');
+
+  function getPageSize() {
+    return Math.max(
+      document.body.clientHeight,
+      document.body.scrollHeight,
+      document.documentElement.scrollHeight,
+      document.documentElement.clientHeight
+    );
+  }
+
+  function getVerticalCenter() {
+    var screenHeight = (window.orientation === 0) ? screen.availHeight : screen.availWidth;
+    var height = Math.min(screenHeight, window.innerHeight);
+
+    return document.body.scrollTop + (height / 2);
+  }
+
+  function Tutorial(element) {
+    this.element = $(element);
+    this.init();
+  }
+
+  Tutorial.prototype = {
+    init: function() {
+      this.tutorial = $("#js-tutorial");
+      this.section1 = $("#js-tutorial-section1");
+      this.section2 = $("#js-tutorial-section2");
+      this.addEvent();
+      this.handleOpen();
+    },
+
+    addEvent: function() {
+      var $btnNext = $("#js-btn-tutorial-next");
+      var $btnEnd = $("#js-btn-tutorial-end");
+
+      $btnNext.on("click", this.next.bind(this));
+      $btnEnd.on("click", this.end.bind(this));
+    },
+
+    handleOpen: function() {
+      // TODO: localstrage
+      if (0) return;
+
+      this.open();
+    },
+
+    next: function() {
+      this.section1.hide();
+      this.section2.show();
+    },
+
+    end: function() {
+      overlay
+        .removeClass("js-show")
+        .addClass("js-hide");
+      this.tutorial
+        .removeClass("js-show")
+        .addClass("js-hide");
+    },
+
+    open: function() {
+      overlay
+        .removeClass("js-hide")
+        .addClass("js-show");
+      this.tutorial
+        .removeClass("js-hide")
+        .addClass("js-show");
+      this.center();
+      overlay.css({height: getPageSize() + 'px'});
+    },
+
+    center: function() {
+      var vHeight = getVerticalCenter();
+      var mHeight = this.tutorial[0].offsetHeight;
+      var h = vHeight - mHeight/2;
+      if (h < 0) {
+        h = 0;
+      }
+      this.tutorial.css({top: h + 'px'});
+    }
+ };
+
+ window.document.addEventListener('DOMContentLoaded', function() {
+   new Tutorial();
+ });
+
+})(window, $);
